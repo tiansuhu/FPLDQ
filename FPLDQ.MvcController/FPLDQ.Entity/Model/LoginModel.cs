4d28e1bc2aa3df4b5400cdb4f4908550eead9798
h3user <h3user@DESKTOP-1QF8JUK>
2019-02-12 10:38:13 +0800

    创建Model 目录 用来添加程序中的Model 实体类

---
+++ FPLDQ.MvcController/FPLDQ.Entity/Model/LoginModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPLDQ.Entity
+{
+    //==============================================================
+    //  作者：tianxy
+    //  时间：2019/2/12 10:37:00
+    //  文件名：LoginModel
+    //  版本：V1.0.1  
+    //  说明： 
+    //  修改者：tianxy
+    //  修改说明： 
+    //==============================================================
+    class LoginModel
+    {
+    }
+}
